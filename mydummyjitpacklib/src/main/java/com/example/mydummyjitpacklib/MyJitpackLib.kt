e7fee8fac438c4f6d87e9e42751abdfb5ee3470e
Steffen Funke <info@steffen-funke.de>
2018-03-12 09:22:33 +0100

    Initial commit

---
+++ mydummyjitpacklib/src/main/java/com/example/mydummyjitpacklib/MyJitpackLib.kt
@@ -0,0 +1,12 @@
+package com.example.mydummyjitpacklib
+
+/**
+ * Created by Steffen Funke on 12.03.18.
+ */
+class MyJitpackLib {
+    fun hello(): String {
+        val message = "Hello from Library"
+        println(message)
+        return message
+    }
+}
